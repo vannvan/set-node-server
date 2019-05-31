25f845cfe1b7a9f5607d6607cfb689b9b7a425bc
shiwan <shiwanwan@thinkjoy.cn>
2019-05-31 18:29:39 +0800

    解决静态资源无法加载的问题

--- bin/index.js
+++ bin/index.js
@@ -1,42 +1,13 @@
 #!/usr/bin/env node
-const os = require('os');
 const http = require('http');
 const fs = require('fs');
-
-
-var interfaces = os.networkInterfaces();
-
+const Ip = require('../lib/get-ip')
+const F = require('../lib/get-file')
 const root = process.cwd();  //当前目录
+const port = Math.floor(Math.random () * 1000) + 8000;
 
-const port = '8081';
+var localIP = Ip.getlocalIp()
 
-var localIP = getlocalIp()
-
-//获取ip
-function getlocalIp(){
-	var interfaces = os.networkInterfaces();
-	var ipArr = []
-    for (var devName in interfaces) {
-        var iface = interfaces[devName];
-        for (var i = 0; i < iface.length; i++) {
-            var alias = iface[i];
-            if (alias.family == 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
-                // return alias.address;
-                ipArr.push(alias.address)
-            }
-        }
-    }
-    return ipArr
-}
-
-//获取文件类型
- function getType(file){
-    var filename=file;
-    var index1=filename.lastIndexOf(".");
-    var index2=filename.length;
-    var type=filename.substring(index1+1,index2);
-    return type;
- }
 //创建本地服务
 async function setHttpServer() {
 	await setConsoleInfo();
@@ -50,35 +21,47 @@ async function setHttpServer() {
 }
 
 var server=http.createServer(function(req,res){
-    var url=req.url;
-    var file = root+url;
-    fs.readFile(file,function(err,data){
-        if(err){
-            res.writeHeader(404,{
-                'content-type' : 'text/html;charset="utf-8"'
-            });
-            res.write(`${file}`)
-            let fileList =  fs.readdirSync(`${file}`);
-            for(let f of fileList){
-            	let type = getType(f)
-            	if(type=='html'){
-            		res.write('<br/>'+`<a href="${f}">${f}</a>`+'\n')
-            	}
+    if(req.url === '/favicon.ico') {
+        // console.log('\033[42;30m DONE \033[40;32m Compiled successfully in 19987ms\033[0m')
+    }else{
+        var url=req.url;
+        var file = root+url;
+        fs.readFile(file,function(err,data){
+            if(err){
+                res.writeHeader(404,{
+                    'content-type' : 'text/html;charset="utf-8"'
+                });
+                if(!F.countFile(F.geFileList(root)).html){
+                    res.write('<h1>404页面</h1><p><h2>当前目录没有html文件</h2>')
+                }else{
+                    let fileList =  fs.readdirSync(`${file}`);
+                    for(let f of fileList){
+                        let type = F.getType(f)
+                        if(type=='html'){
+                            res.write('<br/>'+`<a href="${f}">${f}</a>`+'\n')
+                        }
+                    }
+                }
+                res.end();
+            }else{
+                var surl = '.'+url;
+                var type = surl.substr(surl.lastIndexOf(".")+1,surl.length)
+                res.writeHeader(200,{
+                    'content-type' : "text/"+type+';'+'charset="utf-8"'
+                });
+                res.write(data);
+                res.end();
             }
-            res.end();
-        }else{
-            res.writeHeader(200,{
-                'content-type' : 'text/html;charset="utf-8"'
-            });
-            res.write(data);//将index.html显示在客户端
-            res.end();
-        }
-    })
+        })  
+    } 
 }).listen(port);
+
+
 
 //显示本地服务地址
 function setConsoleInfo(){
-	console.log('服务已开启')
+  let info = 'The default service has been opened in the browser!'
+	console.log('\033[42;30m DONE \033[;32m' + info + '\033[0m')
 	for(let dev of localIP){
 		console.log(`${dev}`+':'+port)
 	}
@@ -103,3 +86,5 @@ function openDefaultBrowser(url) {
 }
 
 setHttpServer();
+
+
